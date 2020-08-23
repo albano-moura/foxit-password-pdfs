52733a772afffc6f19b93c8c33b6aaa72d57ba14
Karl Hughes <khughes.me@gmail.com>
2020-08-23 08:16:55 -0500

    Removing log

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(process.env.LICENSE_SN)
   res.render('index', {
     licenseSN: process.env.LICENSE_SN,
     licenseKey: process.env.LICENSE_KEY,
